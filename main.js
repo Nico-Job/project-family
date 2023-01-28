aee42b2714dcd2a2f4dff983feb60241284b9953
Nico-Job <86542247+Nico-Job@users.noreply.github.com>
2023-01-28 12:52:05 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,21 @@ function take_snapshot()
          }
          ); }
           console.log('ml5 version:', ml5.version);
-            classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
+            classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/dsZ9S1aKX/model.json',modelLoaded);
+            function modelLoaded() 
+             { console.log('Model Loaded!');
+             }
+              function check() {
+                 img = document.getElementById('selfie_image');
+                  classifier.classify(img, gotResult);
+                 }
+function gotResult(error, results) 
+{
+     if (error) 
+     {
+         console.error(error); 
+        } 
+        else { 
+            console.log(results);
+             document.getElementById("result_family_name").innerHTML = results[0].label;
+             } }
